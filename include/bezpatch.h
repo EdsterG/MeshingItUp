5b8911cd9c636f2b1ece9db46933823a1173bad1
Wisam Reid <wisamreid@gmail.com>
2014-11-20 05:03:49 -0800

    bezpatchinterp and bezcurveinterp

--- include/bezpatch.h
+++ include/bezpatch.h
@@ -3,36 +3,31 @@
 
 #include "point.h"
 #include <iostream>
+#include "vertex.h"
+#include "bezpatch.h"
 
 // stl :: string theory labs (taking over a universe near you)
 namespace stl
 {
 
+// point / derivative
+typedef struct {
+  Point p;
+  Point d;
+} PointDeriv ;
+
 class BezPatch {
- private:
+  private:
 
-  Point control[4][4];
+    // PointDeriv PN;
 
-  // point / derivative
-  struct PD{
-    Point point;
-    Point derivative;
-  };
+  public:
 
-  // point / normal
-  struct PN{
-    Point point;
-    Point normal;
-  };
+    BezPatch();
 
-  PD bezcurveinterp(Point (&curve)[4], double u);
-  PN bezpatchinterp(Point (&patch)[4][4], double u, double v);
-
- public:
-  BezPatch();
-
+    PointDeriv bezcurveinterp(Point (&curve)[4], double u);
+    Vertex bezpatchinterp(Point (&patch)[4][4], double u, double v);
 };
 
 }
-
 #endif
--- src/bezpatch.cpp
+++ src/bezpatch.cpp
@@ -10,10 +10,9 @@ Eddie Groshev cs184-en
 using namespace stl;
 
 
-
-PDN BezPatch::bezcurveinterp(Point (&curve)[4], double u){
+PointDeriv BezPatch::bezcurveinterp(Point (&curve)[4], double u){
   Point A, B, C, D, E;
-  PD   
+  PointDeriv PD;
 
   // split each of the three segments
   // to form two new ones AB and BC
@@ -26,15 +25,41 @@ PDN BezPatch::bezcurveinterp(Point (&curve)[4], double u){
   E = B * (1.0-u) + C * u;
 
   // this is the point on the curve
-  P = D * (1.0-u) + E * u
+  PD.p = D * (1.0-u) + E * u;
 
   // compute derivative
-  dPdu = 3 * (E - D)
+  PD.d = (E - D) * 3;
 
-  return PD
-
-
-
+  return PD;
 
 };
-PDN BezPatch::bezpatchinterp(Point& patch[4][4], double u, double v);
+
+Vertex BezPatch::bezpatchinterp(Point (&patch)[4][4], double u, double v){
+
+  Point n;
+  PointDeriv uPD, vPD;
+  Point vcurve[4];
+  Point ucurve[4];
+
+  // build control points
+  for(int i = 0; i < 4; i++){
+    // build control points for a Bezier curve in v
+    vcurve[0] = bezcurveinterp(patch[0], u).p;
+
+    // build control points for a Bezier curve in v
+    Point tempCurve[4];
+    tempCurve[0] = patch[0][i];
+    tempCurve[1] = patch[1][i];
+    tempCurve[2] = patch[2][i];
+    tempCurve[3] = patch[3][i];
+    ucurve[i] = bezcurveinterp(tempCurve, v).p;
+  }
+  // evaluate surface and derivative for u and v
+  vPD = bezcurveinterp(vcurve, v);
+  uPD = bezcurveinterp(ucurve, u);
+  // take cross product of partials to find normal
+  n = (uPD.d).cross(vPD.d);
+  n = n.norm();
+
+  return Vertex(uPD.p, n);
+};
